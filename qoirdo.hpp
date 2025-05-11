3ee14b7ffdaa30f8ff66be6a5c5b7e90e6c9e1a5
Green Sky <Green-Sky@users.noreply.github.com>
2025-05-11 13:07:30 +0200

    patch v2 still slow due to the oklab pretty clean now, still has "param" internally

--- qoirdo.hpp
+++ qoirdo.hpp
@@ -1,6 +1,8 @@
-// basisu_resampler_filters.h
-// Copyright (C) 2019 Binomial LLC. All Rights Reserved.
-//
+#pragma once
+
+// qoirdo.hpp
+// Copyright (C) 2022 Richard Geldreich, Jr. All Rights Reserved.
+// Copyright (C) 2025 Erik Scholz
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -12,24 +14,19 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-#pragma once
 
-#include "basisu.h"
+#include <cstdint>
+#include <vector>
 
-namespace basisu
-{
-	typedef float (*resample_filter_func)(float t);
+struct qoi_rdo_desc {
+	unsigned int width;
+	unsigned int height;
+	unsigned char channels;
+	unsigned char colorspace;
+};
 
-	struct resample_filter
-	{
-		const char *name;
-		resample_filter_func func;
-		float support;
-	};
+// quality 1-100
+std::vector<uint8_t> encode_qoi_rdo_simple(const uint8_t* data, const qoi_rdo_desc& desc, int quality);
 
-	extern const resample_filter g_resample_filters[];
-	extern const int g_num_resample_filters;
-
-	int find_resample_filter(const char *pName);
-
-} // namespace basisu
+// TODO: finetuneable
+//uint8_t* encode_qoi_rdo_advanced(const uint8_t* data, const qoi_rdo_desc* desc, int* out_len);
